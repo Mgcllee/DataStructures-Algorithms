9a18a96e3ecd6001533abc9e40abe3dbdb767475
mgcllee <mgcllee@gmail.com>
2023-11-08 18:20:18 +0900

    [BAEKJOON][Sol] No.1929
    Sieve of Eratosthenes를 사용한 주어진 범위의 소수 전부 구하기
    밀러, 피터슨 등 다른 소수 판별 알고리즘도 있지만 주어진 범위에서 모든 소수를 출력하므로 에스테라토스의 체가 더 유리

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,108 +1,24 @@
 #include <iostream>
-using namespace std;
 
-struct INFO
+int main() 
 {
-	int grade = 0;
-	bool marked = false;
-};
+	int Min = 0, Max = 0;
+	std::cin >> Min >> Max;
+	bool* PrimeArray = new bool[Max];
 
-int st_cnt = 0, Jo_num = -1, dis = -1, week = 0;
-INFO** grade = new INFO * [week];
+	for (int i = 2; i <= Max; i++)
+		PrimeArray[i] = true;
 
-void Input(int* happy_stack, int* happy_continue_stack, int* Max_happy_ctn_stack)
-{
-	cin >> st_cnt >> Jo_num >> dis >> week;
-	for (int i = 0; i < week; ++i) grade[i] = new INFO[st_cnt];
-
-	grade[0][0].marked = true;
-	grade[0][Jo_num - 1].marked = true;
-
-	if (Jo_num - 1 <= dis) {
-		*happy_stack = 1;
-		*happy_continue_stack = 1;
-		*Max_happy_ctn_stack = 1;
-	}
-
-	int grade_buffer;
-	for (int i = 0; i < week - 1; ++i) {
-		for (int gradeTime = 0; gradeTime < 2; ++gradeTime) {
-			for (int j = 0; j < st_cnt; ++j) {
-				cin >> grade_buffer;
-				grade[i][j].grade += ((gradeTime == 0) ? grade_buffer : -grade_buffer);
-			}
-		}
-	}
-}
-void CompareAndSwap(int cur_week, int left_index, int right_index)
-{
-	bool goodGradePlus = grade[cur_week][left_index].grade >= 0;
-	bool badGradePlus = grade[cur_week][right_index].grade >= 0;
-
-	if (goodGradePlus && !badGradePlus) return;
-	else if ((goodGradePlus && badGradePlus && (grade[cur_week][left_index].grade + 2 <= grade[cur_week][right_index].grade))
-		|| (!goodGradePlus && badGradePlus)
-		|| (!goodGradePlus && !badGradePlus && (grade[cur_week][left_index].grade + 4 <= grade[cur_week][right_index].grade))) {
-
-		grade[cur_week][left_index].grade += 2;
-		grade[cur_week][right_index].grade -= 2;
-
-		INFO swap_buf;
-		for (int i = cur_week; i < week - 1; ++i) {
-			swap_buf = grade[cur_week][left_index];
-			grade[cur_week][left_index] = grade[cur_week][right_index];
-			grade[cur_week][right_index] = swap_buf;
-		}
-	}
-}
-
-void GradeJudge(int curr_week, int* prev_happy_week, int* happy_stack, int* happy_continue_stack, int* Max_happy_ctn_stack) {
-	int sumDist = st_cnt;
-	for (int find_st = 0; find_st < st_cnt; ++find_st) {
-		if (grade[curr_week][find_st].marked) {
-			grade[curr_week + 1][find_st].marked = true;
-			if (sumDist == st_cnt) {
-				sumDist = -find_st;
-			}
-			else {
-				sumDist += find_st;
-				break;
-			}
+	for (int i = 2; i <= Max; i++)
+	{
+		if (PrimeArray[i])
+		{
+			for (int j = i + i; j <= Max; j += i)
+				PrimeArray[j] = false;
 		}
 	}
 
-	if (sumDist <= dis) {
-		if (*prev_happy_week == (curr_week - 1)) {
-			*happy_continue_stack += 1;
-		}
-		else *happy_continue_stack = 1;
-
-		if (*Max_happy_ctn_stack <= *happy_continue_stack) {
-			*Max_happy_ctn_stack = *happy_continue_stack;
-		}
-
-		*prev_happy_week = curr_week;
-		*happy_stack += 1;
-	}
-	else *happy_continue_stack = 0;
-}
- 
-int main()
-{
-	int prev_happy_week = -1;
-	int happy_stack = 0;
-	int happy_ctn_stack = 0;
-	int Max_happy_ctn_stack = 0;
-
-	Input(&happy_stack, &happy_ctn_stack, &Max_happy_ctn_stack);
-
-	for (int cur_week = 0; cur_week < week - 1; ++cur_week) {
-		for (int cmp_num = 0; cmp_num < st_cnt - 1; ++cmp_num) {
-			CompareAndSwap(cur_week, cmp_num, cmp_num + 1);
-		}
-		GradeJudge(cur_week, &prev_happy_week, &happy_stack, &happy_ctn_stack, &Max_happy_ctn_stack);
-	}
-	
-	printf("%d %d", happy_stack, Max_happy_ctn_stack);
-	return 0;
+	// cout << endl을 사용할 경우 buffer를 지울 때 시간이 소요되어 타임오버
+	for (int i = Min; i <= Max; ++i)
+		if (PrimeArray[i] && i >= 2) printf("%d\n", i);
 }
