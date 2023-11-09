b185aa0155a6c99522766f119258c64c039bbd58
mgcllee <mgcllee@gmail.com>
2023-11-10 00:18:01 +0900

    [BAEKJOON][idea] No.9095
    multiset의 정렬과 중복허용을 이용해 순열 계산

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <set>
 
 int main() 
 {
-	int Min = 0, Max = 0;
-	std::cin >> Min >> Max;
-	bool* PrimeArray = new bool[Max];
+	std::multiset<int> con = { 3, 2, 4, 1, 2, 4, 1, 3, 2, 2 };
+	for (auto N : con)
+		std::cout << N << ", ";
 
-	for (int i = 2; i <= Max; i++)
-		PrimeArray[i] = true;
-
-	for (int i = 2; i <= Max; i++)
-	{
-		if (PrimeArray[i])
-		{
-			for (int j = i + i; j <= Max; j += i)
-				PrimeArray[j] = false;
-		}
+	int temp = 0, cur = 0;
+	while (*con.begin() != 3) {
+		temp = *(con.rbegin() - cur);
+		if (temp <= 3) break;
+		else ++cur;
 	}
 
-	// cout << endl을 사용할 경우 buffer를 지울 때 시간이 소요되어 타임오버
-	for (int i = Min; i <= Max; ++i)
-		if (PrimeArray[i] && i >= 2) printf("%d\n", i);
+	*(con.rbegin() - cur) = 1;
+
+	return 0;
+
+
+	int testCnt, N;
+	std::cin >> testCnt;
+	std::vector<int> cal;
+	
+	for (int i = 0, result = 0; i < testCnt; ++i, result = 0) {
+		std::cin >> N;	// N의 최대값은 11
+		
+		for (int j = 0; j < N; ++j)	cal.push_back( 1 );
+		/*int temp = cal.back();
+		cal.pop_back();*/
+		
+		int temp = 0, cur = 1;
+		while (*cal.begin() != 3) {
+			temp = *(cal.end() - cur);
+			
+		}
+		
+
+		for (auto N : cal)
+			std::cout << N << ", ";
+		std::cout << std::endl;
+
+		// printf("%d\n", result);
+	}
 }
