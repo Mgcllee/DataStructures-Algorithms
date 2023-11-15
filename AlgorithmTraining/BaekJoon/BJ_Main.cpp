95e2519c12354392d1b13e020983ebc0dd6ca618
mgcllee <mgcllee@gmail.com>
2023-11-15 15:17:34 +0900

    [BAEKJOON][preSol] No.1520
    DFS(Depth First Searching)를 사용한 내리막길 경로 탐색

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <queue>
 
-
 int dijkstra(std::vector<std::pair<int, int>> edge[], int* dis, int start, int end) {
 	dis[start] = 0;
 	std::priority_queue<std::pair<int, int>> pq;
@@ -36,6 +35,20 @@ int dijkstra(std::vector<std::pair<int, int>> edge[], int* dis, int start, int e
 	return cnt;
 }
 
+bool visited[9];
+std::vector<std::vector<int>> adj;
+void dfs(int x)
+{
+	visited[x] = true;
+	std::cout << x << " ";
+	for (int i = 0; i < adj[x].size(); i++) // 인접한 노드 사이즈만큼 탐색
+	{
+		int y = adj[x][i];
+		if (!visited[y]) // 방문하지 않았으면 즉 visited가 False일 때 not을 해주면 True가 되므로 아래 dfs 실행
+			dfs(y); // 재귀적으로 방문
+	}
+}
+
 int main() 
 {
 	int M, N;
@@ -51,12 +64,8 @@ int main()
 
 	for (int i = 0, num = 1; i < M; ++i) {
 		for (int j = 0; j < N && num <= N * M; ++j, ++num) {
-			// 좌, 상 노드
-			if ((j != 0) && (arr[i][j] > arr[i][j - 1])) node[num].push_back(std::make_pair(num - 1, std::abs(arr[i][j] - arr[i][j - 1])));
-			if ((i != 0) && (arr[i][j] > arr[i - 1][j])) node[num].push_back(std::make_pair(num - N, std::abs(arr[i][j] - arr[i - 1][j])));
-			// 우, 하 노드
-			if ((j != N - 1) && (arr[i][j] > arr[i][j + 1])) node[num].push_back(std::make_pair(num + 1, std::abs(arr[i][j] - arr[i][j + 1])));
-			if ((i != M - 1) && (arr[i][j] > arr[i + 1][j])) node[num].push_back(std::make_pair(num + N, std::abs(arr[i][j] - arr[i + 1][j])));
+			// 좌측이동 가능성 확인
+			if (0 < j && (arr[i][j - 1] < arr[i][j])) adj[num].push_back()
 		}
 	}
 
