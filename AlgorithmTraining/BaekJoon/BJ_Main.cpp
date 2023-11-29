576af98401ccd5f1bc76c73740d4b20a0e8347c8
Mgcllee <mgcllee@gmail.com>
2023-11-29 12:22:49 +0900

    [BAEKJOON][Sol] No.15650 조합을 사용한 수열문제 풀이

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,23 +1,18 @@
 ﻿#include <iostream>
 #include <vector>
 
-void Permutation(bool* visited, std::vector<short>& vec, std::vector<short>& res, short cnt)
+void Combination(int depth, std::vector<int>& res, std::vector<int>& vec, int next)
 {
-	if (cnt == static_cast<short>(res.size())) {
-		for (short n : res) {
+	if (depth == res.size()) {
+		for (int n : res)
 			printf("%d ", n);
-		}
 		printf("\n");
 		return;
-	}
+	}	
 
-	for (short i = 0; i < static_cast<short>(vec.size()); ++i) {
-		if (visited[i] == true) continue;
-
-		visited[i] = true;
-		res[cnt] = vec[i];
-		Permutation(visited, vec, res, cnt + 1);
-		visited[i] = false;
+	for (int i = next; i < vec.size(); ++i) {
+		res[depth] = vec[i];
+		Combination(depth + 1, res, vec, i + 1);
 	}
 }
 
@@ -25,19 +20,11 @@ int main() {
 	int N, M;
 	std::cin >> N >> M;
 
-	std::vector<short> vec;	// 1부터 N까지의 숫자 정보
-	std::vector<short> res(M);	// 순열로 구한 순열 결과
+	std::vector<int> vec;
+	std::vector<int> res(M);
+	for (int i = 1; i <= N; ++i) vec.push_back(i);
 
-	// 숫자 정보 입력
-	for (short i = 1; i <= N; ++i) {
-		vec.push_back(i);
-	}
-
-	// 방문 기록 남기기
-	bool* visited = new bool[N];
-	for (int i = 0; i < N; ++i) visited[i] = false;
-
-	Permutation(visited, vec, res, 0);
+	Combination(0, res, vec, 0);
 
 	return 0;
 }
