9af62a04e1f14f56cce0a64edd8652a99498390f
Mgcllee <mgcllee@gmail.com>
2023-12-08 17:02:19 +0900

    [preSol][BAEKJOON] #1966 multi set에서 비교연산자를 수정해 중요도와 출력순서를 참고할 수 있도록 설계

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-// #include <map>
+#include <set>
 
 /*
 Queue의 FIFO를 사용하기 때문에
@@ -7,15 +7,30 @@ Queue의 FIFO를 사용하기 때문에
 (Pop된 값이 Push인지 아닌지 파악하기 위해)
 */
 
+struct myOrder
+{
+	bool operator() (const std::pair<int, int>& left, const std::pair<int, int>& right) const
+	{
+		return left.first > right.first;
+	}
+};
+
 int main() {
 	int testCnt = 0;
 	std::cin >> testCnt;
 
 	for (int i = 0; i < testCnt; ++i) {
-		// std::multimap<int, int, std::greater<int>> map;
 		int cnt, target;
 		std::cin >> cnt >> target;
-
+		std::multiset<std::pair<int, int>, myOrder> set;
 		
+		for (int j = 0, temp; j < cnt; ++j) {
+			std::cin >> temp;
+			set.insert(std::make_pair(temp, j));
+		}
+
+		for (auto p : set) {
+			printf("(%d, %d), ", p.first, p.second);
+		}	printf("\n");
 	}
 }
