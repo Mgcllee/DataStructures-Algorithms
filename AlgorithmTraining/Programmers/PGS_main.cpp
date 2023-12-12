52d3c63d79dc9c5273ed7ad6ef0aac436328a1de
mgcllee <mgcllee@gmail.com>
2023-12-13 02:01:21 +0900

    [Sol][PROGRAMMERS] 베스트앨범 map의 정렬 기준을 변경할 수 있음을 이용.

--- AlgorithmTraining/Programmers/PGS_main.cpp
+++ AlgorithmTraining/Programmers/PGS_main.cpp
@@ -1,41 +1,72 @@
-#pragma once
-
-#include "main.h"
-#include "SortFunctions.h"
-
+#include <iostream>
 #include <vector>
+#include <map>
 
-int main() {
-	int n = 5;
-	int m = 4;
-	vector<int> section = { 1, 3 };
-
-	int answer = 0;
-
-	vector<int>::iterator startPos = section.begin();
-	vector<int>::iterator endPos = section.begin();
-
-	/*for ( ; ; startPos = endPos + 1, endPos = endPos + 2) {
-		if (*startPos && (endPos == section.end() || endPos == section.end() - 1)) {
-			*startPos = -1;
-			answer += 1;
-			break;
-		}
-		else if (*startPos && *endPos && (*endPos - *startPos + 1 <= m)) {
-			answer += 1;
-			*startPos = *endPos = -1;
-		}
-	}*/
-
-	for (vector<int>::iterator it = section.begin(); it != section.end(); ++it) {
-		if (it + 1 == section.end()) break;
-
-		/*for (int i = *it; i < *(it + 1); ++i) {
-			cout << i << endl;
-		}
-		cout << "==========\n";*/
-		answer += 1;
+struct gOrder {
+	bool operator() (const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const
+	{
+		return lhs.first > rhs.first;
 	}
-	
-	cout << answer;
+};
+
+struct oriOrder {
+	bool operator() (const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const
+	{
+		if (lhs.first == rhs.first) {
+			return lhs.second < rhs.second;
+		}
+		return lhs.first > rhs.first;
+	}
+};
+
+int main()
+{
+	std::vector<std::string> genres{ "a", "b", "b", "c", "c" };
+	std::vector<int> plays{ 5, 5, 40, 5, 5 };
+
+	std::multimap<std::pair<int, int>, std::pair<std::string, int>, gOrder> map;
+	for (int i = 0; i < genres.size(); ++i) {
+		map.insert({ {plays[i], i}, {genres[i], plays[i]} });
+	}
+
+	std::vector<std::string> sorted;
+	{
+		std::map<std::string, int> sum;
+		for (auto p : map) {
+			if (false == sum.insert(p.second).second) {
+				sum.find(p.second.first)->second += p.second.second;
+			}
+		}
+
+		std::map<int, std::string, std::greater<int>> sortedString;
+		for (auto p : sum) {
+			sortedString.insert({ p.second, p.first });
+		}
+
+
+
+		for (auto p : sortedString) {
+			sorted.push_back(p.second);
+		}
+
+	}
+
+	std::vector<int> result;
+	for (auto p : sorted) {
+		int cnt = 0;
+		std::map<std::pair<int, int>, std::string, oriOrder> temp;
+		for (auto mp : map) {
+			if (p == mp.second.first) {
+				temp.insert({ {mp.first.first, mp.first.second }, mp.second.first });
+			}
+		}
+
+		for (auto p : temp) {
+			result.push_back(p.first.second);
+			if (++cnt == 2) break;
+		}
+	}
+
+	for (int i : result)
+		std::cout << i << " ";
 }
