4c23e886395a3d893cfd7dfa2f873d8090df4dfe
Mgcllee <mgcllee@gmail.com>
2023-12-13 12:51:13 +0900

    [Sol][BAEKJOON] #2193 다이나믹 프로그래밍과 메모이제이션을 함께 사용하여 실행 속도 향상

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,30 +1,25 @@
 ﻿#include <iostream>
-#include <vector>
 
-void ComPerm(std::vector<int> num, std::vector<int> res, int cnt)
+int size; 
+long long cnt;
+long long DP[91];
+
+long long Com(int currSize)
 {
-	if (cnt == res.size()) {
-		for (int n : res)
-			printf("%d ", n);
-		printf("\n");
-		return;
+	if (2 >= currSize) {
+		return 1;
 	}
 
-	for (int i = 0; i < num.size(); ++i) {
-		res[cnt] = num[i];
-		ComPerm(num, res, cnt + 1);
+	if (DP[currSize] != 0) {
+		return DP[currSize];
 	}
+
+	return DP[currSize] = Com(currSize - 2) + Com(currSize - 1);
 }
 
 int main()
 {
-	int cnt, size;
-	std::cin >> cnt >> size;
-	std::vector<int> num;
-	for (int i = 1; i <= cnt; ++i) num.push_back(i);
-	std::vector<int> res(size);
-
-	ComPerm(num, res, 0);
-
+	std::cin >> size;
+	std::cout << Com(size);
 	return 0;
 }
