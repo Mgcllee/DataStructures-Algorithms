8ba60237f0914ad53cabe0ec5a580a0e4850e07c
mgcllee <mgcllee@gmail.com>
2023-12-18 15:16:14 +0900

    [preSol][BAEKJOON] #2108 최빈값을 구하는 부분이 미해결

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,28 +1,58 @@
 ﻿#include <iostream>
-#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
 
-int maxSum = 0;
-int cnt = 0;
-
-void schedule(int day, std::vector<std::pair<int, int>> vec, int sum) {
-	if (day > cnt) return;
-
-	maxSum = maxSum >= sum ? maxSum : sum;
-	for (int i = day; i < cnt; ++i) {
-		schedule(vec[i].first + i, vec, sum + vec[i].second);
+struct ORDER {
+	bool operator() (const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const
+	{
+		if (lhs.first > rhs.first) return true;
+		else if (lhs.first < rhs.first) return false;
+		else {
+			return lhs.second > lhs.first;
+		}
 	}
-}
+};
 
 int main()
 {
-	std::cin >> cnt;
-	std::vector<std::pair<int, int>> vec;
-	for (int i = 0, days, price; i < cnt; ++i) {
-		std::cin >> days >> price;
-		vec.push_back({ days, price });
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int N, firstVal = 0;
+	std::cin >> N;
+	std::map<int, int> map;
+	std::multiset<std::pair<int, int>, ORDER> set;
+	for (int i = 0, temp; i < N; ++i) {
+		std::cin >> temp;
+		if (i == 0) firstVal = temp;
+		if (false == map.insert({ temp, 1 }).second) {
+			map.find(temp)->second += 1;
+		}
 	}
 
-	schedule(0, vec, 0);
+	long long sum = 0;
+	int index = 0, cenVal = 0;
+	for (auto p : map) {
+		sum += (p.first * p.second);
+		index += p.second;
+		if (index == N / 2 + 1) cenVal = p.first;
+		set.insert({p.second, p.first});
+	}
 
-	std::cout << maxSum;
+	auto first_p = *set.begin();
+	for (auto p : set) {
+		if (first_p != p && first_p.first == p.first && first_p.first > p.first) {
+			first_p = p;
+		}
+		
+	}
+
+	printf("%d\n", (int)round((float)sum / (float)N));
+	printf("%d\n", cenVal);
+	printf("%d\n", (set.size() == N) ? firstVal : (--set.rbegin())->second);
+	printf("%d\n", (*map.rbegin()).first - (*map.begin()).first);
+
+	return 0;
 }
