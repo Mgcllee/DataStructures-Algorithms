9374503e4e126a637f3a108d1c9c13ae5df24565
Mgcllee <mgcllee@gmail.com>
2023-12-19 12:44:26 +0900

    [preSol][BAEKJOON] #1654 (시간: 28분) 탐색시간이 너무 오래 걸려 문제 발생

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,54 +1,36 @@
 ﻿#include <iostream>
 #include <cmath>
-#include <map>
-#include <set>
 
-struct ORDER {
-	bool operator() (const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const
-	{
-		if (lhs.first > rhs.first) return true;
-		else if (lhs.first < rhs.first) return false;
-		else {
-			return lhs.second > lhs.second;
+int main() {
+	int K = 0, N = 0, sum = 0;
+	std::cin >> K >> N;
+	int* arr = new int[K];
+
+	for (int t = 0; t < K; ++t) {
+		std::cin >> arr[t];
+		sum += arr[t];
+	}
+
+	int maxVal = sum / N;
+	int minVal = maxVal;
+	int pos = 0;
+	for (; minVal >= 10; ++pos) minVal /= 10;
+	minVal = minVal * pow(10, pos);
+
+	int maxCnt = 0;
+	int maxLen = 0;
+	for (int v = maxVal; v >= minVal; --v) {
+		int currCnt = 0;
+
+		for (int i = 0; i < K; ++i) {
+			currCnt += (arr[i] / v);
+		}
+		if (currCnt > maxCnt){
+			maxCnt = currCnt;
+			maxLen = v;
 		}
 	}
-};
 
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
-
-	int N, maxCnt = 1;
-	std::cin >> N;
-	std::map<int, int> map;
-	std::multiset<std::pair<int, int>, ORDER> set;
-	for (int i = 0, temp; i < N; ++i) {
-		std::cin >> temp;
-		if (false == map.insert({ temp, 1 }).second) {
-			temp = map.find(temp)->second += 1;
-			if (temp > maxCnt) maxCnt = temp;
-		}
-	}
-
-	double sum = 0;
-	int index = 0, cenVal = -4001;
-	for (auto p : map) {
-		sum += (p.first * p.second);
-		index += p.second;
-		if (index == N / 2 + 1 || (cenVal == -4001 && (index > N / 2 + 1))) cenVal = p.first;
-		if (maxCnt == p.second)
-			set.insert({ p.second, p.first });
-	}
-
-	double x = sum / N;
-	double avg = round(x);
-	if (avg == -0) avg = 0;
-	printf("%d\n", (int)avg);
-	printf("%d\n", cenVal);
-	printf("%d\n", (set.size() > 1 ? (++set.begin())->second : set.begin()->second));
-	printf("%d\n", (*map.rbegin()).first - (*map.begin()).first);
-
+	std::cout << maxLen;
 	return 0;
 }
