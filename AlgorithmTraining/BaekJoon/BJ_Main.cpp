50adbf517f60fd8e0dfb6c0ef41e3531d25eeead
mgcllee <mgcllee@gmail.com>
2023-12-20 17:53:01 +0900

    [Sol][BAEKJOON] #2839 그리디 알고리즘을 사용할 수 있지만 문제 특징에 맞춰 더 간단하게 풀이 가능

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,54 +1,22 @@
 ﻿#include <iostream>
-#include <cmath>
-#include <map>
-#include <set>
 
-struct ORDER {
-	bool operator() (const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const
-	{
-		if (lhs.first > rhs.first) return true;
-		else if (lhs.first < rhs.first) return false;
-		else {
-			return lhs.second > lhs.second;
-		}
-	}
-};
-
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
-
-	int N, maxCnt = 1;
+int main() {
+	int N, cnt = 0;
 	std::cin >> N;
-	std::map<int, int> map;
-	std::multiset<std::pair<int, int>, ORDER> set;
-	for (int i = 0, temp; i < N; ++i) {
-		std::cin >> temp;
-		if (false == map.insert({ temp, 1 }).second) {
-			temp = map.find(temp)->second += 1;
-			if (temp > maxCnt) maxCnt = temp;
+
+	while (true) {
+		if (N % 5 == 0) {
+			cnt += N / 5;
+			printf("%d", cnt);
+			break;
+		}
+
+		N -= 3;
+		cnt += 1;
+
+		if (N < 0) {
+			printf("-1");
+			break;
 		}
 	}
-
-	double sum = 0;
-	int index = 0, cenVal = -4001;
-	for (auto p : map) {
-		sum += (p.first * p.second);
-		index += p.second;
-		if (index == N / 2 + 1 || (cenVal == -4001 && (index > N / 2 + 1))) cenVal = p.first;
-		if (maxCnt == p.second)
-			set.insert({ p.second, p.first });
-	}
-
-	double x = sum / N;
-	double avg = round(x);
-	if (avg == -0) avg = 0;
-	printf("%d\n", (int)avg);
-	printf("%d\n", cenVal);
-	printf("%d\n", (set.size() > 1 ? (++set.begin())->second : set.begin()->second));
-	printf("%d\n", (*map.rbegin()).first - (*map.begin()).first);
-
-	return 0;
 }
