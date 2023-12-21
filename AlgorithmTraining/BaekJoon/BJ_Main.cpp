c375b3af5595ef34040d2d0b824b1cb7a48de884
Mgcllee <mgcllee@gmail.com>
2023-12-21 14:44:07 +0900

    [Sol][BAEKJOON] #1978 에스테라토스의 채를 사용하여 소수를 구함

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,36 +1,22 @@
 ﻿#include <iostream>
-#include <cmath>
 
 int main() {
-	int K = 0, N = 0, sum = 0;
-	std::cin >> K >> N;
-	int* arr = new int[K];
+	int* prime = new int[1001];
 
-	for (int t = 0; t < K; ++t) {
-		std::cin >> arr[t];
-		sum += arr[t];
+	prime[1] = 0;
+	for (int i = 2; i <= 1'000; ++i) prime[i] = i;
+	for (int i = 2; i <= 1'000; ++i) {
+		if (prime[i] == 0) continue;
+		for (int j = i + i; j <= 1'000; j += i) prime[j] = 0;
 	}
 
-	int maxVal = sum / N;
-	int minVal = maxVal;
-	int pos = 0;
-	for (; minVal >= 10; ++pos) minVal /= 10;
-	minVal = minVal * pow(10, pos);
-
-	int maxCnt = 0;
-	int maxLen = 0;
-	for (int v = maxVal; v >= minVal; --v) {
-		int currCnt = 0;
-
-		for (int i = 0; i < K; ++i) {
-			currCnt += (arr[i] / v);
-		}
-		if (currCnt > maxCnt){
-			maxCnt = currCnt;
-			maxLen = v;
-		}
+	int N, cnt = 0;
+	std::cin >> N;
+	for (int i = 0, temp; i < N; ++i) {
+		std::cin >> temp;
+		if (prime[temp]) cnt += 1;
 	}
 
-	std::cout << maxLen;
+	printf("%d", cnt);
 	return 0;
 }
