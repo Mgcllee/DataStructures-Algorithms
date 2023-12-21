4b1bd336c97043e42161e9df1646f04c861de887
Mgcllee <mgcllee@gmail.com>
2023-12-21 15:51:24 +0900

    [preSol][BAEKJOON] #18111 주어진 블록의 최고 위치보다 높게 쌓을 수 있는 경우를 고려하지 못함

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,22 +1,41 @@
 ﻿#include <iostream>
+#include <cmath>
 
 int main() {
-	int* prime = new int[1001];
+	// N: 세로길이, M: 가로길이, B: 인벤내부 수
+	int N, M, B;
+	std::cin >> N >> M >> B;
+	int** arr = new int*[N];
+	for (int i = 0; i < N; ++i) arr[i] = new int[M];
 
-	prime[1] = 0;
-	for (int i = 2; i <= 1'000; ++i) prime[i] = i;
-	for (int i = 2; i <= 1'000; ++i) {
-		if (prime[i] == 0) continue;
-		for (int j = i + i; j <= 1'000; j += i) prime[j] = 0;
+	long long sum = 0;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			std::cin >> arr[i][j];
+			sum += arr[i][j];
+		}
 	}
+	int avg = std::round(sum / (N * M));
 
-	int N, cnt = 0;
-	std::cin >> N;
-	for (int i = 0, temp; i < N; ++i) {
-		std::cin >> temp;
-		if (prime[temp]) cnt += 1;
+	while (avg >= 0) {
+		long long usedBlock = 0;
+		long long saveBlock = 0;
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < M; ++j) {
+				int dis = arr[i][j] - avg;
+				if (dis > 0)
+					saveBlock += dis;
+				else if (dis < 0)
+					usedBlock -= dis;
+			}
+		}
+
+		if (saveBlock + B >= usedBlock) {
+			long long time = saveBlock * 2 + usedBlock;
+			printf("%lld %d", time, avg);
+			break;
+		}
+		else avg -= 1;
 	}
-
-	printf("%d", cnt);
 	return 0;
 }
