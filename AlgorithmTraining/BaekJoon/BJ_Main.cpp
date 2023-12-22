b74615ec7939e72d97c3c699dd0090f3587202e4
Mgcllee <mgcllee@gmail.com>
2023-12-22 13:23:16 +0900

    [preSol][BAEKJOON] #1874 입력된 수열을 스택의 특징을 사용해 구현하는 문제

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,63 +1,30 @@
 ﻿#include <iostream>
-
-class NODE {
-	NODE* next;
-	int val;
-
-public:
-	NODE(int inV) : val(inV), next(nullptr) { }
-	
-	int get() { return val; }
-	int set(int v) { val = v; }
-	void set_next(NODE* nn) { next = nn; }
-	NODE* get_next() { return next; }
-};
+#include <vector>
 
 int main() {
-	int N = 0;
-	std::cin >> N;
-	NODE* top = nullptr;
-	int size = 0;
-
-	for (int i = 0; i < N; ++i) {
-		std::string order;
-		std::cin >> order;
-		if (order == "push") {
-			int newNum;
-			std::cin >> newNum;
-			NODE* newNode = new NODE(newNum);
-			
-			if (top == nullptr) {
-				top = newNode;
-			}
-			else {
-				newNode->set_next(top);
-				top = newNode;
-			}
-			size += 1;
-		}
-		else if (order == "pop") {
-			if (top == nullptr) printf("-1\n");
-			else {
-				printf("%d\n", top->get());
-				NODE* delNode = top;
-				top = top->get_next();
-				delete delNode;
-				size -= 1;
-			}
-		}
-		else if (order == "size") {
-			if (top == nullptr) printf("0\n");
-			else printf("%d\n", size);
-		}
-		else if (order == "empty") {
-			if (top == nullptr) printf("1\n");
-			else printf("0\n");
-		}
-		else if (order == "top") {
-			if (top == nullptr) printf("-1\n");
-			else printf("%d\n", top->get());
-		}
+	int seq;
+	std::cin >> seq;
+	int* arr = new int[seq];
+	for (int i = 0; i < seq; ++i) {
+		std::cin >> arr[seq - 1 - i];
 	}
-	return 0;
+
+	std::vector<char> vec;
+	for (int i = 1; i <= seq; ++i) {
+		if (i == arr[i - 1]) vec.push_back('+');
+		else if (i < arr[i - 1]) {
+			for (int j = i; j <= seq; ++j) {
+				if (j < arr[i - 1]) vec.push_back('+');
+				else {
+					vec.push_back('-');
+					break;
+				}
+			}
+		}
+		else vec.push_back('-');
+	}
+
+	printf("=================\n");
+	for (char c : vec)
+		printf("%c\n", c);
 }
