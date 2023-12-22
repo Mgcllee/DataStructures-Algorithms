571f950c059ce13f1325851c2f403e16d8a5d1f5
Mgcllee <mgcllee@gmail.com>
2023-12-22 12:29:20 +0900

    [Sol][BAEKJOON] #10828 스택 구조를 연결리스트로 직접 구현

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,19 +1,63 @@
-﻿#include<iostream>
+﻿#include <iostream>
+
+class NODE {
+	NODE* next;
+	int val;
+
+public:
+	NODE(int inV) : val(inV), next(nullptr) { }
+	
+	int get() { return val; }
+	int set(int v) { val = v; }
+	void set_next(NODE* nn) { next = nn; }
+	NODE* get_next() { return next; }
+};
 
 int main() {
-	int T;
-	std::cin >> T;
-	
-	for (int i = 0; i < T; ++i) {
-		std::string buf;
-		std::cin >> buf;
-		int lcnt = 0, rcnt = 0;
-		for (char c : buf) {
-			if (c == '(') lcnt += 1;
-			else rcnt += 1;
-			if (lcnt < rcnt) break;
+	int N = 0;
+	std::cin >> N;
+	NODE* top = nullptr;
+	int size = 0;
+
+	for (int i = 0; i < N; ++i) {
+		std::string order;
+		std::cin >> order;
+		if (order == "push") {
+			int newNum;
+			std::cin >> newNum;
+			NODE* newNode = new NODE(newNum);
+			
+			if (top == nullptr) {
+				top = newNode;
+			}
+			else {
+				newNode->set_next(top);
+				top = newNode;
+			}
+			size += 1;
 		}
-		if (lcnt == rcnt) printf("YES\n");
-		else printf("NO\n");
+		else if (order == "pop") {
+			if (top == nullptr) printf("-1\n");
+			else {
+				printf("%d\n", top->get());
+				NODE* delNode = top;
+				top = top->get_next();
+				delete delNode;
+				size -= 1;
+			}
+		}
+		else if (order == "size") {
+			if (top == nullptr) printf("0\n");
+			else printf("%d\n", size);
+		}
+		else if (order == "empty") {
+			if (top == nullptr) printf("1\n");
+			else printf("0\n");
+		}
+		else if (order == "top") {
+			if (top == nullptr) printf("-1\n");
+			else printf("%d\n", top->get());
+		}
 	}
+	return 0;
 }
