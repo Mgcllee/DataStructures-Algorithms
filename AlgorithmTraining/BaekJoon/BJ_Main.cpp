b3c625edbff00de4300a562bd67b34d8eb335015
mgcllee <mgcllee@gmail.com>
2023-12-25 13:43:35 +0900

    [Sol][BAEKJOON] #18110 list의 양방향 push 특성을 이용한 문제풀이

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,35 +1,30 @@
 ﻿#include <iostream>
-#include <set>
-
-struct WORD_ORDER {
-	bool operator() (const std::string& lhs, const std::string& rhs) const
-	{
-		if (lhs.size() < rhs.size()) {
-			return true;
-		}
-		else if (lhs.size() > rhs.size()) {
-			return false;
-		}
-
-		for (int i = 0; i < lhs.size(); ++i) {
-			if (lhs[i] == rhs[i]) continue;
-			return lhs[i] < rhs[i];
-		}
-		return false;
-	}
-};
+#include <list>
+#include <cmath>
 
 int main() {
-	int N;
-	std::cin >> N;
-	std::set<std::string, WORD_ORDER> set;
-	for (int i = 0; i < N; ++i) {
-		std::string buf;
-		std::cin >> buf;
-		set.insert(buf);
+	int n;
+	std::cin >> n;
+	if (n == 0) {
+		printf("0\n");
+		return 0;
 	}
-	
-	for (auto s : set)
-		std::cout << s << std::endl;
+
+	std::list<int> list;
+	for (int i = 0, temp; i < n; ++i) {
+		std::cin >> temp;
+		list.push_back(temp);
+	}
+	list.sort();
+
+	int expt_cnt = (int)std::round((float)n * 0.3f * 0.5f);
+	for (int i = 0; i < expt_cnt; ++i) {
+		if (false == list.empty()) list.pop_front();
+		if (false == list.empty()) list.pop_back();
+	}
+	float sum = 0;
+	for (int n : list)
+		sum += n;
+	printf("%d", (int)(std::round(sum / list.size())));
 	return 0;
 }
