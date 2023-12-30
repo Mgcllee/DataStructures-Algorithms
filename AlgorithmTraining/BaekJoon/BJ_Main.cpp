21ce0ad42ada751d71095c95ec102194d5275e38
mgcllee <mgcllee@gmail.com>
2023-12-30 15:22:58 +0900

    [Solved.ac][A16] 아레나 2번

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,33 +1,37 @@
 ﻿#include <iostream>
 
 int main() {
-	int T;
-	std::cin >> T;
+	int N, K, cnt = 0;
+	std::cin >> N >> K;
+	
+	
+	int* arr = new int[N];
+	for (int i = 0; i < N; ++i) {
+		std::cin >> arr[i];
+	}
+	
+	// 일단 입력은 다 받아야 함.
+	if (N == 1) {
+		cnt = 0;
+	}
+	else {
+		int prev = arr[0];
 
-	for (int t = 0; t < T; ++t) {
-		int N, M, K, cnt = 0;
-		std::cin >> N >> M >> K;
-
-		if (K == 1 && M == 1 && N != 1) {
-			printf("-1\n");
-			continue;
-		}
-
-		while (0 < N) {
-			cnt += 1;
-			
-			if (N >= M * K) {
-				N -= (M * K);
-				if (N > 0) {
-					cnt += 1;
-					N += 1;
-				}
+		for (int i = 1; i < N; ++i) {
+			if (prev < arr[i]) {
+				prev = arr[i];
 			}
-			else break;
+			else if (prev < arr[i] + K) {
+				cnt += 1;
+				prev = arr[i] + K;
+			}
+			else {
+				cnt = -1;
+				break;
+			}
 		}
-
-		printf("%d\n", cnt);
 	}
 
+	printf("%d", cnt);
 	return 0;
 }
