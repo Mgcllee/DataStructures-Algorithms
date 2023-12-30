a70b05b545d991a547ec2fb4ca5f0bfed6d3d027
mgcllee <mgcllee@gmail.com>
2023-12-30 17:34:12 +0900

    [preSol][A16] 아레나 문제 솔브닥 아레나16의 A-3, C-1 번 문제 시도 내용

--- AlgorithmTraining/BaekJoon/BJ_Main.cpp
+++ AlgorithmTraining/BaekJoon/BJ_Main.cpp
@@ -1,37 +1,113 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <vector>
+//#include <set>
+//
+//struct GRADE {
+//	bool operator() (const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const
+//	{
+//		if ((lhs.first > rhs.first) && (lhs.second > rhs.second)) return false;
+//		else return true;
+//	}
+//};
+//
+//int main() {
+//	int N;
+//	std::cin >> N;
+//	std::vector<int> vec;
+//
+//	std::multiset<std::pair<int, int>, GRADE> set;
+//	for (int i = 0, a, b; i < N; ++i) {
+//		std::cin >> a >> b;
+//		set.insert(std::make_pair(a, b));
+//	}
+//
+//	int curr = 0;
+//	std::pair<int, int> prev = { -1, -1 };
+//	for (auto p : set) {
+//		if (prev.first == -1) {
+//			prev = p;
+//			vec.push_back(1);
+//		}
+//		else {
+//			if (prev.first < p.first && prev.second < p.second) {
+//				prev = p;
+//				curr += 1;
+//				vec.push_back(1);
+//			}
+//			else vec[curr] += 1;
+//		}
+//	}
+//
+//	std::multiset<int> s_set{ vec.begin(), vec.end() };
+//	int cnt[3] = { 0, 0, 0 }, i = 0;
+//	for (int n : s_set) {
+//		cnt[i] += n;
+//		if (i + 1 < 3) ++i;
+//		else i = 0;
+//	}
+//
+//	if (vec.size() < 3) printf("-1");
+//	else printf("%d", cnt[0] - cnt[2]);
+//	
+//	return 0;
+//}
 
-int main() {
-	int N, K, cnt = 0;
-	std::cin >> N >> K;
-	
-	
-	int* arr = new int[N];
-	for (int i = 0; i < N; ++i) {
-		std::cin >> arr[i];
-	}
-	
-	// 일단 입력은 다 받아야 함.
-	if (N == 1) {
-		cnt = 0;
-	}
-	else {
-		int prev = arr[0];
 
-		for (int i = 1; i < N; ++i) {
-			if (prev < arr[i]) {
-				prev = arr[i];
-			}
-			else if (prev < arr[i] + K) {
-				cnt += 1;
-				prev = arr[i] + K;
+
+#include <iostream>
+#include <list>
+
+bool check_pair(std::string buf) {
+	std::string check_str;
+	bool answer = true;
+	for (char c : buf) {
+		if (c == '(' || c == '[') {
+			check_str.push_back(c);
+			answer = false;
+		}
+		else if (false == check_str.empty()) {
+
+			if ((*check_str.rbegin() == '(' && c == ')')
+				|| (*check_str.rbegin() == '[' && c == ']')) {
+				check_str.pop_back();
+				answer = true;
 			}
 			else {
-				cnt = -1;
+				answer = false;
 				break;
 			}
 		}
+		else {
+			answer = false;
+			break;
+		}
+	}
+	if (answer && check_str.empty()) return true;
+	else return false;
+}
+
+int main() {
+	int N;
+	std::string buf;
+	std::cin >> N >> buf;
+	std::list<char> str{ buf.begin(), buf.end() };
+
+	while (!str.empty()) {
+		std::list lhs = str, rhs = str;
+		lhs.pop_front();
+		rhs.pop_back();
+		std::string ls{lhs.begin(), lhs.end()};
+		std::string rs{rhs.begin(), rhs.end()};
+		if (check_pair(ls) || check_pair(rs)) {
+			printf("jimin");
+			return 0;
+		}
+		else {
+			if (!str.empty()) str.pop_back();
+			if (!str.empty()) str.pop_front();
+		}
 	}
 
-	printf("%d", cnt);
+	printf("junhui");
 	return 0;
 }
